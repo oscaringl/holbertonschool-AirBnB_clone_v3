2e6a0f94672414ad139c9094632f8ed20116716a
Oscar Guerra <oogl29@gmail.com>
2023-05-06 00:00:12 -0500

    Update file app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
-"""
-starts a Flask web app.
-"""
-
-from flask import Flask, jsonify, Response
-from api.v1.views import app_views
-from models import storage
+"""Version 1 of the HBNB REST api"""
+from flask import Flask, jsonify
 from flask_cors import CORS
-import os
+from models import storage
+from api.v1.views import app_views
+from os import getenv
 
-app = Flask(__name__)
+app = Flask('v1')
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-cors = CORS(app, resources={'/*': {'origins': '0.0.0.0'}})
-
-
-@app.teardown_appcontext
-def teardown_appcontext(self):
-    return storage.close()
+CORS(app, resources=r"/api/v1/*", origins="*")
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    '''
-        Returns a JSON-formatted 404 status code response.
-    '''
+def not_found(error):
+    """Handle 404 not found errors and return json object"""
     return jsonify({"error": "Not found"}), 404
 
 
-if __name__ == '__main__':
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=os.getenv('HBNB_API_PORT', 5000), threaded=True)
+@app.teardown_appcontext
+def close_storage(*args, **kwargs):
+    """Clost app storage (FileStorage or DBStorage)"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=int(port))
